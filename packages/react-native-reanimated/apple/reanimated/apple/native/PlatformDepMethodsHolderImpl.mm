f5f959f5a202858811f904adcd473d2285517ee7
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2026-06-12 10:04:16 +0200

    Prepare platform transition routing for more properties

--- packages/react-native-reanimated/apple/reanimated/apple/native/PlatformDepMethodsHolderImpl.mm
+++ packages/react-native-reanimated/apple/reanimated/apple/native/PlatformDepMethodsHolderImpl.mm
@@ -1,4 +1,5 @@
 #import <reanimated/Tools/PlatformDepMethodsHolder.h>
+#import <reanimated/apple/CSS/REACSSPlatformProps.h>
 #import <reanimated/apple/CSS/REACSSPlatformTransitions.h>
 #import <reanimated/apple/READisplayLink.h>
 #import <reanimated/apple/REANodesManager.h>
@@ -128,7 +129,12 @@ KeyboardEventUnsubscribeFunction makeUnsubscribeFromKeyboardEventsFunction(REAKe
 
 css::CSSCanRoutePropertyFunction makeCSSCanRouteProperty()
 {
-  return &canRouteCSSProperty;
+  return &css::canRouteCSSProperty;
+}
+
+css::CSSParseValueFunction makeCSSParseValue()
+{
+  return &css::parsePlatformValue;
 }
 
 css::CSSApplyTransitionFunction makeCSSApplyTransition(REACSSPlatformTransitions *platformTransitions)
@@ -141,8 +147,7 @@ css::CSSApplyTransitionFunction makeCSSApplyTransition(REACSSPlatformTransitions
 css::CSSRemoveTransitionFunction makeCSSRemoveTransition(REACSSPlatformTransitions *platformTransitions)
 {
   return [platformTransitions](Tag viewTag, const std::string &propertyName) {
-    [platformTransitions removeTransitionForTag:viewTag
-                                   propertyName:[NSString stringWithUTF8String:propertyName.c_str()]];
+    [platformTransitions removeTransitionForTag:viewTag propertyName:propertyName];
   };
 }
 
@@ -210,6 +215,7 @@ PlatformDepMethodsHolder makePlatformDepMethodsHolder(RCTModuleRegistry *moduleR
   REACSSPlatformTransitions *platformTransitions =
       [[REACSSPlatformTransitions alloc] initWithSurfacePresenter:nodesManager.surfacePresenter];
   auto cssCanRouteProperty = makeCSSCanRouteProperty();
+  auto cssParseValue = makeCSSParseValue();
   auto cssApplyTransition = makeCSSApplyTransition(platformTransitions);
   auto cssRemoveTransition = makeCSSRemoveTransition(platformTransitions);
 
@@ -227,6 +233,7 @@ PlatformDepMethodsHolder makePlatformDepMethodsHolder(RCTModuleRegistry *moduleR
       attachPseudoSelectorFunction,
       detachPseudoSelectorFunction,
       cssCanRouteProperty,
+      cssParseValue,
       cssApplyTransition,
       cssRemoveTransition,
   };
